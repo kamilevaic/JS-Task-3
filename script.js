a317b1da4bb4b22ef2386b1fef4835916b4a3fe6
kamilevaic <106099595+kamilevaic@users.noreply.github.com>
2022-10-08 16:26:42 +0300

    task complete

--- script.js
+++ script.js
@@ -11,4 +11,31 @@ Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
+
+document.getElementById("btn").addEventListener("click", showUsers);
+
+function generateHtml(card) {
+  const userCard = document.createElement("div");
+  userCard.setAttribute("class", "user-card");
+  const image = document.createElement("img");
+  image.setAttribute("class", "user-img");
+  const login = document.createElement("id");
+  login.setAttribute("class", "user-login");
+
+  login.innerHTML = card.login;
+  image.src = card.avatar_url;
+
+  document.getElementById("output").append(userCard);
+  userCard.append(image, login);
+}
+
+async function showUsers() {
+  const res = await fetch("https://api.github.com/users");
+  const resJson = await res.json();
+  resJson.forEach((card) => {
+    generateHtml(card);
+  });
+  const hideMsg = document.getElementById("message");
+  hideMsg.remove();
+}
